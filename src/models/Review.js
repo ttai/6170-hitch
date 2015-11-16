6e39824ad17b383e880f8d992c90a9ac70e150d7
ttai <ttai@wellesley.edu>
2015-11-16 15:52:20 -0500

    review functions

--- src/models/Review.js
+++ src/models/Review.js
@@ -1,2 +1,34 @@
+var mongoose = require('mongoose');
+var schemas = require('./schemas');
+var userSchema = schemas.userSchema;
+var rideSchema = schemas.rideSchema;
+var reviewSchema = schemas.reviewSchema;
+var User = require('./User');
 
-Review.js
+var Review = (function Review() {
+
+var that = Object.create(Review.prototype);
+
+	that.getReview = function(){
+	}
+
+	that.addReview = function() {
+
+	}
+
+	that.editReview = function() {
+
+	}
+
+	that.removeReview = function() {
+		
+	}
+
+	// Get all the reviews for a user
+	that.getUserReviews = function(){
+
+	}
+
+})()
+
+module.exports = Review;
