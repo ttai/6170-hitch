5933096c5e4c1341e6b7e938ce9962d2abdc6b25
Eric Chen <chen1994tw@jello.local>
2015-11-15 21:57:19 -0500

    add schemas

---
+++ src/models/schemas.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId;
+
+var userSchema = Schema({
+  kerberos: String,
+  reviews: [{ type: ObjectId, ref: 'reviewSchema' }],
+  rides: [{ type: ObjectId, ref: 'rideSchema' }]
+});
+
+var rideSchema = Schema({
+  origin: String,
+  destination: String,
+  departure_time: Date,
+  capacity: Number,
+  creator: { type: ObjectId, ref: 'userSchema' },
+  riders: [{ type: ObjectId, ref: 'userSchema' }],
+  passphrase: String
+});
+
+var reviewSchema = Schema({
+  ride: { type: ObjectId, ref: 'rideSchema' },
+  reviewer: { type: ObjectId, ref: 'userSchema' },
+  reviewee: { type: ObjectId, ref: 'userSchema' },
+  rating: Number,
+  comment: String
+});
+
+exports.userSchema = userSchema;
+exports.rideSchema = rideSchema;
+exports.reviewSchema = reviewSchema;
