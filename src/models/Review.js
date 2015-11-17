665d466cbd099d5f0343f83f33e02093653afaed
Brandon <bzeng@mit.edu>
2015-11-17 01:41:14 -0500

    initial implementation for review model

--- src/models/Review.js
+++ src/models/Review.js
@@ -8,27 +8,55 @@ var User = require('./User');
 var Review = (function Review() {
 
   var that = Object.create(Review.prototype);
-    that.getReview = function(){
-      
-    }
 
-    that.addReview = function() {
+    that.getReview = function(reviewID, callback){
+        
+    };
 
-    }
+    that.addReview = function(rideID, reviewerID, revieweeID, 
+      rating, comment, callback) {
+        reviewSchema.create({
+          "ride": rideID,
+          "reviewer": reviewerID,
+          "reviewee": revieweeID,
+          "rating": rating,
+          "comment": comment
+        })
+    };
 
-    that.editReview = function() {
+    that.setReviewRating = function(reviewID, rating, callback) {
+      reviewSchema.find({"_id": reviewID}, {}, function(e, doc) {
+        reviewSchema.update({"_id": reviewID}, {$set: {"rating": rating}},
+          function(e, subdoc) {
+          });
+      });
+    };
 
-    }
+    that.setReviewComment = function(reviewID, comment, callback) {
+      reviewSchema.find({"_id": reviewID}, {}, function(e, doc) {
+        reviewSchema.update({"_id": reviewID}, {$set: {"comment": comment}}, 
+          function(e, subdoc) {
+        });
+      });
+    };
+    
+    that.deleteReview = function(reviewID, callback) {
+      reviewSchema.find({"_id": reviewID}).remove(function(e, doc) {
+      });      
+    };
 
-    that.removeReview = function() {
-          
-    }
+    // Get all the reviews for a user (reviewee)
+    that.getUserReviews = function(userID, callback){
+      reviewSchema.find({"reviewee": userID}, {}, function(e, doc) {
+        if (e) {
+          callback("error")
+        } else {
+          callback(null, doc)
+        }
+      });
+    };
 
-      // Get all the reviews for a user
-    that.getUserReviews = function(){
-
-    }
-         
-})()
+    return that;
+})();
    
 module.exports = Review;
