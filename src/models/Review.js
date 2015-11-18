a7a69a9c4947befc923ea19a75f7e59787dc42de
Eric Chen <chen1994tw@jello.local>
2015-11-18 12:08:08 -0500

    Review.js

--- src/models/Review.js
+++ src/models/Review.js
@@ -10,51 +10,60 @@ var Review = (function Review() {
   var that = Object.create(Review.prototype);
 
     that.getReview = function(reviewID, callback){
-        
+      reviewSchema.findOne({ "_id": reviewID }, function(err, review) {
+        if (err) {
+          // TODO: implement error handling
+        }
+        callback(null, review);
+      });
     };
 
     that.addReview = function(rideID, reviewerID, revieweeID, 
-      rating, comment, callback) {
-        reviewSchema.create({
-          "ride": rideID,
-          "reviewer": reviewerID,
-          "reviewee": revieweeID,
-          "rating": rating,
-          "comment": comment
-        }, {}, function(e, doc) {
-          userSchema.find({"_id": revieweeID}, {}, function(e, subdoc) {
-            userSchema.update({"_id": revieweeID}, {$push: {"reviews": doc._id}}, 
-              function(e, subsubdoc) {
-            })
-          })
+                              rating, comment, callback) {
+      reviewSchema.create({
+        "ride": rideID,
+        "reviewer": reviewerID,
+        "reviewee": revieweeID,
+        "rating": rating,
+        "comment": comment
+      }, {}, function(e, doc) {
+        userSchema.find({"_id": revieweeID}, {}, function(e, subdoc) {
+          userSchema.update({"_id": revieweeID}, {$push: {"reviews": doc._id}}, 
+                            function(err, subsubdoc) {
+            // TODO: implement error handling
+          });
         });
+      });
     };
 
     that.setReviewRating = function(reviewID, rating, callback) {
-      reviewSchema.find({"_id": reviewID}, {}, function(e, doc) {
+      reviewSchema.find({"_id": reviewID}, {}, function(err, doc) {
         reviewSchema.update({"_id": reviewID}, {$set: {"rating": rating}},
-          function(e, subdoc) {
-          });
+                            function(err, subdoc) {
+          // TODO: implement error handling
+        });
       });
     };
 
     that.setReviewComment = function(reviewID, comment, callback) {
-      reviewSchema.find({"_id": reviewID}, {}, function(e, doc) {
+      reviewSchema.find({"_id": reviewID}, {}, function(err, doc) {
         reviewSchema.update({"_id": reviewID}, {$set: {"comment": comment}}, 
-          function(e, subdoc) {
+                            function(err, subdoc) {
+          // TODO: implement error handling
         });
       });
     };
     
     that.deleteReview = function(reviewID, callback) {
-      reviewSchema.find({"_id": reviewID}).remove(function(e, doc) {
+      reviewSchema.find({"_id": reviewID}).remove(function(err, doc) {
+        // TODO: implement error handling
       });      
     };
 
     // Get all the reviews for a user (reviewee)
     that.getUserReviews = function(userID, callback){
-      reviewSchema.find({"reviewee": userID}, {}, function(e, doc) {
-        if (e) {
+      reviewSchema.find({"reviewee": userID}, {}, function(err, doc) {
+        if (err) {
           callback("error")
         } else {
           callback(null, doc)
@@ -62,6 +71,7 @@ var Review = (function Review() {
       });
     };
 
+    Object.freeze(that);
     return that;
 })();
    
