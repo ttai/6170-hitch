82ab0ef10ca003dd3be113e5646a03174600d236
Eric Chen <chen1994tw@jello.local>
2015-11-18 15:04:03 -0500

    trimming

--- src/models/Review.js
+++ src/models/Review.js
@@ -27,48 +27,31 @@ var Review = (function Review() {
         "rating": rating,
         "comment": comment
       }, {}, function(e, doc) {
-        userSchema.find({"_id": revieweeID}, {}, function(e, subdoc) {
-          userSchema.update({"_id": revieweeID}, {$push: {"reviews": doc._id}}, 
-                            function(err, subsubdoc) {
-            // TODO: implement error handling
-          });
+        userSchema.update({"_id": revieweeID}, {$push: {"reviews": doc._id}}, 
+                          function(err) {
+          // TODO: implement error handling
         });
       });
     };
 
     that.setReviewRating = function(reviewID, rating, callback) {
-      reviewSchema.find({"_id": reviewID}, {}, function(err, doc) {
-        reviewSchema.update({"_id": reviewID}, {$set: {"rating": rating}},
-                            function(err, subdoc) {
-          // TODO: implement error handling
-        });
+      reviewSchema.update({"_id": reviewID}, {$set: {"rating": rating}},
+                          function(err) {
+        // TODO: implement error handling
       });
     };
 
     that.setReviewComment = function(reviewID, comment, callback) {
-      reviewSchema.find({"_id": reviewID}, {}, function(err, doc) {
-        reviewSchema.update({"_id": reviewID}, {$set: {"comment": comment}}, 
-                            function(err, subdoc) {
-          // TODO: implement error handling
-        });
+      reviewSchema.update({"_id": reviewID}, {$set: {"comment": comment}}, 
+                          function(err) {
+        // TODO: implement error handling
       });
     };
     
     that.deleteReview = function(reviewID, callback) {
-      reviewSchema.find({"_id": reviewID}).remove(function(err, doc) {
+      reviewSchema.find({"_id": reviewID}).remove(function(err) {
         // TODO: implement error handling
       });      
-    };
-
-    // Get all the reviews for a user (reviewee)
-    that.getUserReviews = function(userID, callback){
-      reviewSchema.find({"reviewee": userID}, {}, function(err, doc) {
-        if (err) {
-          callback("error")
-        } else {
-          callback(null, doc)
-        }
-      });
     };
 
     Object.freeze(that);
