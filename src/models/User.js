77620a80c50127d945071a75601faceee2a73595
Selina <leungs@mit.edu>
2015-11-18 15:03:47 -0500

    fixed merge conflicts

--- src/models/User.js
+++ src/models/User.js
@@ -2,6 +2,8 @@
 var mongoose = require("mongoose");
 var schemas = require("./schemas");
 var userSchema = schemas.userSchema;
+var Review = require('./Review');
+var Ride = require('./Ride');
 
 var User = (function User() {
 
@@ -9,85 +11,85 @@ var User = (function User() {
   //returns the user object if it exists
 
   that.userExists = function(currentKerberos,callback){
-  	userSchema.find({kerberos: currentKerberos}, function(err, user){
-		if (err){
-			doesExist= err;
-		}
-		else if (user == null){
-			doesExist=false;
-		}
-		else{
-			doesExist=true;
-		}
-		callback(doesExist,user);
-	});
+    userSchema.find({kerberos: currentKerberos}, function(err, user){
+    if (err){
+      doesExist= err;
+    }
+    else if (user == null){
+      doesExist=false;
+    }
+    else{
+      doesExist=true;
+    }
+    callback(doesExist,user);
+  });
   }
 
   //password or certificate authentication 
   that.createUser = function(currentKerberos, callback){
-  	userExists(currentKerberos,function(doesExist, user){
-  		if (doesExist){
-  			callback("username taken");
-  		}else if(currentKerberos===""){
-  			callback("invalid");
-  		}else{
-  			users.create({"kerberos": currentKerberos}, function(e, info){
-  				if (e){
-  					callback("error");
-  				}else{
-  					callback("valid");
-  				}
-  			});
-  		}  		
-  	})
+    userExists(currentKerberos,function(doesExist, user){
+      if (doesExist){
+        callback("username taken");
+      }else if(currentKerberos===""){
+        callback("invalid");
+      }else{
+        userSchema.create({"kerberos": currentKerberos}, function(e, info){
+          if (e){
+            callback("error");
+          }else{
+            callback("valid");
+          }
+        });
+      }     
+    })
   }
 
   // Adds a user to a specific ride
   that.joinRide= function(currentKerberos, ride1, callback){
-  	userExists(currentKerberos,function(doesExist, user){
-		if (doesExist){
-			users.update({"ride": ride1}, {$push: {"riders": user}}, function(e){});
-		}
-	}); 
+    userExists(currentKerberos,function(doesExist, user){
+    if (doesExist){
+      Ride.update({"ride": ride1}, {$push: {"riders": user}}, function(e){});
+    }
+  }); 
   }
 
   // Deletes a user from a specific ride
   that.leaveRide=function(currentKerberos, ride, callback){
-  	userExists(currentKerberos,function(doesExist, user){
-		if (doesExist){
-			users.update({"ride": ride1}, {$pull: {"riders": user}}, function(e){});
-		}
-	}); 
+    userExists(currentKerberos,function(doesExist, user){
+    if (doesExist){
+      Ride.update({"ride": ride1}, {$pull: {"riders": user}}, function(e){});
+    }
+  }); 
   }
 
   // Give a list of all reviews given to a user
   that.getReviews=function(currentKerberos, callback){
-  	userExists(currentKerberos,function(doesExist, user){
-		if (doesExist) {
-			Review.find({ '_id': user.reviews }, function(err, reviews)) {
-				callback(reviews);
-			}
-		}
-	}); 
+    userExists(currentKerberos,function(doesExist, user){
+    if (doesExist) {
+      Review.find({ '_id': user.reviews }, function(err, reviews)) {
+        callback(reviews);
+      }
+    }
+  }); 
   }
 
   // Gives the user's average rating.
   that.getUserRating=function(currentKerberos, callback){
-  	userExists(currentKerberos,function(doesExist,user){
-  		if (doesExist){
-  			callback(user.rating);
-  		}
-  	})
+    userExists(currentKerberos,function(doesExist,user){
+      if (doesExist){
+        callback(user.rating);
+      }
+    })
   }
 
 
   // do add review!
   that.addReview=function(currentKerberos,review,callback){
-  	userExists(currentKerberos, function(doesExist,user){
-  		if (doesExist){
-  			//TODO: implement this
-  		}
-  	})
+    userExists(currentKerberos, function(doesExist,user){
+      if (doesExist){
+        userSchema.update({"kerberos": currentKerberos}, {$push: {"reviews": review}}, function(e){});
+      }
+    })
   }
 
 
