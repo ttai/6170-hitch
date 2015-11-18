17b6c3b111f7749febf437761e5db4a8eab1a5b7
Eric Chen <chen1994tw@jello.local>
2015-11-18 15:09:27 -0500

    Review.js

--- src/models/Review.js
+++ src/models/Review.js
@@ -9,53 +9,53 @@ var Review = (function Review() {
 
   var that = Object.create(Review.prototype);
 
-    that.getReview = function(reviewID, callback){
-      reviewSchema.findOne({ "_id": reviewID }, function(err, review) {
-        if (err) {
-          // TODO: implement error handling
-        }
-        callback(null, review);
-      });
-    };
+  that.getReview = function(reviewID, callback){
+    reviewSchema.findOne({ "_id": reviewID }, function(err, review) {
+      if (err) {
+        // TODO: implement error handling
+      }
+      callback(null, review);
+    });
+  };
 
-    that.addReview = function(rideID, reviewerID, revieweeID, 
-                              rating, comment, callback) {
-      reviewSchema.create({
-        "ride": rideID,
-        "reviewer": reviewerID,
-        "reviewee": revieweeID,
-        "rating": rating,
-        "comment": comment
-      }, {}, function(e, doc) {
-        userSchema.update({"_id": revieweeID}, {$push: {"reviews": doc._id}}, 
-                          function(err) {
-          // TODO: implement error handling
-        });
-      });
-    };
-
-    that.setReviewRating = function(reviewID, rating, callback) {
-      reviewSchema.update({"_id": reviewID}, {$set: {"rating": rating}},
-                          function(err) {
+  that.addReview = function(rideID, reviewerID, revieweeID, 
+                            rating, comment, callback) {
+    reviewSchema.create({
+      "ride": rideID,
+      "reviewer": reviewerID,
+      "reviewee": revieweeID,
+      "rating": rating,
+      "comment": comment
+    }, {}, function(e, doc) {
+      userSchema.update({"_id": revieweeID}, {$push: {"reviews": doc._id}}, 
+                        function(err) {
         // TODO: implement error handling
       });
-    };
+    });
+  };
 
-    that.setReviewComment = function(reviewID, comment, callback) {
-      reviewSchema.update({"_id": reviewID}, {$set: {"comment": comment}}, 
-                          function(err) {
-        // TODO: implement error handling
-      });
-    };
-    
-    that.deleteReview = function(reviewID, callback) {
-      reviewSchema.find({"_id": reviewID}).remove(function(err) {
-        // TODO: implement error handling
-      });      
-    };
+  that.setReviewRating = function(reviewID, rating, callback) {
+    reviewSchema.update({"_id": reviewID}, {$set: {"rating": rating}},
+                        function(err) {
+      // TODO: implement error handling
+    });
+  };
 
-    Object.freeze(that);
-    return that;
+  that.setReviewComment = function(reviewID, comment, callback) {
+    reviewSchema.update({"_id": reviewID}, {$set: {"comment": comment}}, 
+                        function(err) {
+      // TODO: implement error handling
+    });
+  };
+  
+  that.deleteReview = function(reviewID, callback) {
+    reviewSchema.find({"_id": reviewID}).remove(function(err) {
+      // TODO: implement error handling
+    });      
+  };
+
+  Object.freeze(that);
+  return that;
 })();
    
 module.exports = Review;
