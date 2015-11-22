99f2a0faa9dbc1a0d163ed778f6a76b721ae199f
Eric Chen <chen1994tw@jello.local>
2015-11-22 15:40:45 -0500

    add validators to schemas

--- src/models/schemas.js
+++ src/models/schemas.js
@@ -4,29 +4,29 @@ var Schema = mongoose.Schema;
 var ObjectId = Schema.Types.ObjectId;
 
 var userSchema = Schema({
-  kerberos: String,
-  password: String,
+  kerberos: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
   rating: Number,
   reviews: [{ type: ObjectId, ref: 'reviewSchema' }],
   rides: [{ type: ObjectId, ref: 'rideSchema' }]
 });
 
 var rideSchema = Schema({
-  origin: String,
-  destination: String,
-  departure_time: Date,
-  total_capacity: Number,
-  remaining_capacity: Number,
+  origin: { type: String, required: true },
+  destination: { type: String, required: true },
+  departure_time: { type: Date, required: true },
+  total_capacity: { type: Number, required: true },
+  remaining_capacity: { type: Number, required: true },
   riders: [{ type: ObjectId, ref: 'userSchema' }],
   transport: String,
   passphrase: String
 });
 
 var reviewSchema = Schema({
-  ride: { type: ObjectId, ref: 'rideSchema' },
-  reviewer: { type: ObjectId, ref: 'userSchema' },
-  reviewee: { type: ObjectId, ref: 'userSchema' },
-  rating: Number,
+  ride: { type: ObjectId, ref: 'rideSchema', required: true },
+  reviewer: { type: ObjectId, ref: 'userSchema', required: true },
+  reviewee: { type: ObjectId, ref: 'userSchema', required: true },
+  rating: { type: Number, required: true },
   comment: String
 });
 
