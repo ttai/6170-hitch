30031c0b555ea99c8d72de310172e8213183f4e1
Brandon <bzeng@mit.edu>
2015-11-22 19:13:56 -0500

    added sorting by time

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,10 +8,20 @@ var utils = require('../utils/utils');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var currentUser = req.session.currentUser;
+  var sortByDate = function (a, b) {
+    if (a.departure_time < b.departure_time) {
+      return -1;
+    } else if (a.departure_time > b.departure_time) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
   Ride.getAllOpenRides(function(err, rides) {
     if (err) {
       res.render('error', {'message': 'An unknown error occured', 'error.status': 500})
     } else {
+      rides.sort(sortByDate);
       if (currentUser) {
         res.render('index', {'user': currentUser, 'rides': rides, 'loggedIn': true})
       } else {
