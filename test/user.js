c280b18d923a6abece41b6d75430a3fa5165f701
Eric Chen <chen1994tw@jello.local>
2015-12-04 10:43:43 -0500

    add more user tests

--- test/user.js
+++ test/user.js
@@ -20,31 +20,35 @@ describe('User', function() {
 
   beforeEach(function(done) {
     userModel.remove({}, function() {
-      userModel.create({
-        kerberos: 'user1@mit.edu',
-        password: 'password1',
-        rating: 5,
-        reviews: [],
-        rides: []
-      }, function(err, returned_user1) {
-        userModel.create({
-          kerberos: 'user2@mit.edu',
-          password: 'password2',
-          rating: 5,
-          reviews: [],
-          rides: []
-        }, function(err, returned_user2) {
+      rideModel.remove({}, function() {
+        reviewModel.remove({}, function() {
           userModel.create({
-            kerberos: 'user3@mit.edu',
-            password: 'password3',
+            kerberos: 'user1@mit.edu',
+            password: 'password1',
             rating: 5,
             reviews: [],
             rides: []
-          }, function(err, returned_user3) {
-            user1 = returned_user1;
-            user2 = returned_user2;
-            user3 = returned_user3;
-            done();
+          }, function(err, returned_user1) {
+            userModel.create({
+              kerberos: 'user2@mit.edu',
+              password: 'password2',
+              rating: 5,
+              reviews: [],
+              rides: []
+            }, function(err, returned_user2) {
+              userModel.create({
+                kerberos: 'user3@mit.edu',
+                password: 'password3',
+                rating: 5,
+                reviews: [],
+                rides: []
+              }, function(err, returned_user3) {
+                user1 = returned_user1;
+                user2 = returned_user2;
+                user3 = returned_user3;
+                done();
+              });
+            });
           });
         });
       });
@@ -87,6 +91,40 @@ describe('User', function() {
         assert.equal(users.length, 1);
         assert.equal(users[0].kerberos, user1.kerberos);
         done();
+      });
+    });
+  });
+
+  describe('verifyPassword', function() {
+    it('should check that password is incorrect', function(done) {
+      User.verifyPassword(user1.kerberos, 'asdf', function(err, user) {
+        assert.equal(false, user);
+        done();
+      });
+    });
+    it('should check that password is correct', function(done) {
+       User.verifyPassword(user1.kerberos, 'password1', function(err, user) {
+        assert.equal(user1.kerberos, user.kerberos);
+        done();
+      });
+    });
+  });
+
+  describe('getRides', function() {
+    it('should get 0 rides', function(done) {
+      User.getRides(user1._id, function(err, rides) {
+        assert.equal(rides.length, 0);
+        done();
+      });
+    });
+    it('should get a ride', function(done) {
+      Ride.addRide(user1._id, 'orig', 'dest', Date.now(), 4, 'Uber',
+                   function(err, ride) {
+        User.getRides(user1._id, function(err, rides) {
+          assert.equal(rides.length, 1);
+          // TODO: figure out _id comparison
+          done();
+        });
       });
     });
   });
